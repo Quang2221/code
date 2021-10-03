9f7875b1b9212df05588068698a4c9108fdf326b
Quang Anh <bourtourvinh@gmail.com>
2021-10-03 13:08:01 +0700

    btvn

---
+++ session 5/index.js
@@ -0,0 +1,19 @@
+
+// let a = Number (prompt("enter a")) 
+// let x = a + 1
+// console.log(`x = ${x}`)
+
+// if (a == 4 ) 
+// console.log (`x = 10`)
+
+// let a = Number (prompt("enter A"))
+// let x = a + 1
+
+// if (a == 5){
+//     console.log (`x = 12345`)
+// } else {
+// if (a == 12)
+// console.log (`x=123456789101112`)
+// }
+
+
