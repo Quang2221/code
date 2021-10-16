f9d628c1fdfc7b7c5969fd54736c5ce525ca6001
Quang Anh <bourtourvinh@gmail.com>
2021-10-17 02:54:48 +0700

    ss7hw

--- session 6/index.js
+++ session 6/index.js
@@ -1,20 +1,65 @@
+// let fibonacci_number = function (n) 
+// {
+//   if (n===1) 
+//   {
+//     return [0, 1];
+//   } 
+//   else 
+//   {
+//     let s = fibonacci_number(n - 1);
+//     s.number(s[s.length - 1] + s[s.length - 2]);
+//     return s;
+//   }
+// };
 
-// ex5:
-let fibonacci_number = function (n) 
-{
-  if (n===1) 
-  {
-    return [0, 1];
-  } 
-  else 
-  {
-    let s = fibonacci_number(n - 1);
-    s.number(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
-};
-
- console.log(fibonacci_number(8));
+//  console.log(fibonacci_number(8));
  
 //  ex6
 
+// for (let j = 1; 1 <= 5; j++) {
+//     let oneLine = ``
+//     for (let i = 0; i < j; i++) {
+//         oneLine += j
+
+//     }
+
+//     console.log(oneLine)
+// }
+
+// array
+// let students = [`Q.Anh`, `Son`, `Nam`]
+
+// console.log (students[0])
+
+// // add new array
+// students.push (`New student`)
+// console.log(students)
+
+// remove array
+// students.splice (0,1)
+// console.log(students)
+
+
+// students[2] = `NaN1`
+// console.log (students)
+
+// ICRUD iInit: create Read   update delete
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`Student ${i + 1} is ${students [i]}`)
+// }
+
+let numbers = [1,3,2,6,4,56,43,56,98]
+// for (let i = 0; i < 9; i++) {
+//     const isEven = numbers[i] % 2 === 0
+//     if (isEven) {
+//         console.log(numbers[i])
+//     }
+// }
+
+for (let num of numbers) {
+    const isEven = num % 2 === 0
+    if (isEven) {
+        console.log(num)
+    }
+}
---
+++ session 7/index.js
@@ -0,0 +1,35 @@
+// ex1:let dishes = [`Beef steak`, `Chicken soup`, `Russian salad`, `Orange juice`]
+// console.log (dishes[0]) 
+// {
+//     dishes.push (`Ice Cream`)
+// console.log(dishes)
+// dishes.splice (1,1)
+// console.log(dishes)
+// }
+
+
+
+// exe 4:
+
+
+// const numbers = [1,5,4,2,7,9,5,6,12,3];
+// let result = 0;
+
+// for (let i = 0; i < 10 - 1; i++) {
+//   result += numbers[i];
+// }
+
+// console.log(result)
+
+
+// ex 5:
+
+// let numbers = [1,5,4,2,7,9,5,6,12,3];
+// let highest = numbers[0];
+
+// for (var i = 0; i < 10; i++) {
+//     if (highest < numbers[i] ) {
+//         highest = numbers[i];
+//     }
+// }
+// console.log(highest);
