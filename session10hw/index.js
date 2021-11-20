1f07930500c1651ecf1900b104bec608eb03b87e
Quang Anh <bourtourvinh@gmail.com>
2021-11-21 02:44:21 +0700

    hw

--- session10hw/index.js
+++ session10hw/index.js
@@ -14,20 +14,20 @@
 
 // ex2
 
-// const text = document.getElementById('text')
-// const size = document.getElementById('font size')
-// const color = document.getElementById('color')
-// const backgroundColor = document.getElementById('background color')
-// const change = document.getElementById('change')
-// text.addEventListener('keyup', () =>{
-//     change.innerText = event.target.value
-// })
-// size.addEventListener('keyup', () =>{
-//     change.style.fontSize = `${event.target.value}px`
-// })
-// color.addEventListener('keyup', () =>{
-//     change.style.color = event.target.value
-// })
-// backgroundColor.addEventListener('keyup', () =>{
-//     change.style.backgroundColor = event.target.value
-// })
+const text = document.getElementById('text')
+text.addEventListener('keyup', () =>{
+    change.innerText = event.target.value
+})
+const size = document.getElementById('font size')
+size.addEventListener('keyup', () =>{
+    change.style.fontSize = `${event.target.value}px`
+})
+const color = document.getElementById('color')
+color.addEventListener('keyup', () =>{
+    change.style.color = event.target.value
+})
+const backgroundColor = document.getElementById('background color')
+
+backgroundColor.addEventListener('keyup', () =>{
+    change.style.backgroundColor = event.target.value
+})
